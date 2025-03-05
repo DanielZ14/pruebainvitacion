71a32acccbac9256b77b6f518ef853a3e586dbc0
DanielZ14 <danizr1494@gmail.com>
2025-03-04 22:39:56 -0600

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless'; // Import the Vercel adapter (serverless)
 import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
