cca5689846dbb7b098afbf531d96b42c3dfecb16
DanielZ14 <danizr1494@gmail.com>
2025-03-04 22:33:53 -0600

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'node:url'; // Importante: Asegúrate de tener esta importación
-
+import vercel from '@astrojs/vercel/serverless'; // Import the Vercel adapter
 import tailwindcss from '@tailwindcss/vite';
 
 // astro.config.mjs
@@ -9,5 +9,11 @@ export default defineConfig({
   vite: {
     assetsInclude: ['**/*.csv'],
     plugins: [tailwindcss()]
+    output: 'server', //  Enable server-side rendering
+  adapter: vercel(), // Use the Vercel adapter
   }
 });
+
+import { defineConfig } from 'astro/config';
+
+
