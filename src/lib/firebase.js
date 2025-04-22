d64820dd2f578f1a9bda2c5f5eeb53eacbe3cf49
DanielZ14 <danizr1494@gmail.com>
2025-04-21 23:02:42 -0600

    cambios en fire

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,21 +1,6 @@
 // src/lib/firebase.js
-import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { app } from "../firebase/client";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCyWMkcaqN0pnPRpGOWdubUxkPdL0UyeaE",
-    authDomain: "confirmaciones-de-boda.firebaseapp.com",
-    projectId: "confirmaciones-de-boda",
-    storageBucket: "confirmaciones-de-boda.firebasestorage.app",
-    messagingSenderId: "25804580063",
-    appId: "1:25804580063:web:60bca3aef6f0b8dc4c356a",
-    measurementId: "G-3MW16137D9"
-  };
-  
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export Firestore
+// Export Firestore instance using the existing app
 export const db = getFirestore(app);
-export { firebaseConfig };
